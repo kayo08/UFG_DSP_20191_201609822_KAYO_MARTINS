903e0ed4cc2b1bb0d7efc69830ec8bbaf40abf93
kayo08 <k8martins08@gmail.com>
2019-06-27 22:09:44 -0300

    Xml Student.

---
+++ dsp20191/src/main/java/br/com/kayo08/dsp20191/aulas0912/as/InfoStudent.java
@@ -0,0 +1,53 @@
+package br.com.kayo08.dsp20191.aulas0912.as;
+
+public class InfoStudent {
+
+    private Long matricula;
+    private String firstname;
+    private String lastname;
+    private String nickname;
+    private Frequencia frequencia;
+
+    public InfoStudent (Long matricula, String firstname, String lastname, String nickname, Frequencia frequencia) {
+        this.matricula = matricula;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.frequencia = frequencia;
+    }
+	//Matrícula
+    public Long getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+	//Primeiro Nome
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+	//Ultimo Nome
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+	//Nickname
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+	//Frequencia
+    public Frequencia getFrequencia() {
+        return frequencia;
+    }
+    public void setFrequencia(Frequencia frequencia) {
+        this.frequencia = frequencia;
+    }
+}
